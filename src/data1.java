20ff4ab05515ae0cf0a6b4d065cba924a3ae9538
vshah <vshah2799@gmail.com>
2020-06-15 11:56:20 -0400

    Intial Commit. Just setting up the basics of the code

---
+++ src/data1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class data1 {
+    public static void main(String[] args){
+
+        /*Take in a string with limit of 280 characters. Only letters and spaces allowed. Make the string all lowercase. Put each character of the string into an arraylist.
+        Then randomize which letters get uppercased. Put the updated arraylist back into a string and then return that string.
+         */
+        Scanner scan = new Scanner(System.in);
+        
+
+
+    }
+
+
+
+
+}
